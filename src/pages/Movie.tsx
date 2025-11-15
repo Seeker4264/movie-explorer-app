055c4f7507398d4f2d02d7656d6a94e29b9dfcae
Seeker4264 <sebashmorales@gmail.com>
2025-11-15 15:54:36 -0400

    movie view added

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -1,16 +1,84 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Navbar from "../components/Navbar";
 
+import { testData, singleMovieData } from "../data/testData";
+
+interface Rating {
+  Source: string;
+  Value: string;
+}
+
+interface IMovieDetails {
+  Title: string;
+  Year: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  Language: string;
+  Country: string;
+  Awards: string;
+  Poster: string;
+  Ratings: Rating[];
+  Metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  imdbID: string;
+  Type: string;
+  DVD: string;
+  BoxOffice: string;
+  Production: string;
+  Website: string;
+  Response: string;
+}
+
 const Movie = () => {
   const { movieId } = useParams<{ movieId: string }>();
+  const [movieDetails, setMovieDetails] =
+    useState<IMovieDetails>(singleMovieData);
+
+  useEffect(() => {}, []);
 
   console.log("Movie ID:", movieId);
 
   return (
     <main>
       <Navbar />
-      <section className="flex flex-col justify-center items-center w-full mt-8">
-        <h1>Search: {movieId}</h1>
+      <section className="flex flex-col md:flex-row justify-center items-center w-full mt-16 gap-2 md:gap-16">
+        <div
+          style={{ backgroundImage: `url(${movieDetails.Poster})` }}
+          className="w-72 sm:w-96 h-96 sm:h-128 bg-cover bg-center brightness-50 lg:group-hover:brightness-50 lg:brightness-100 duration-100 rounded-lg"
+        />
+        <div className="md:w-lg max-md:p-15">
+          <h1 className="text-4xl font-bold text-white">
+            {movieDetails.Title} - {movieDetails.Year}
+          </h1>
+          <p className="text-xl font-bold text-[#aaa] mt-2">
+            {movieDetails.Released}
+          </p>
+          <p className="text-md font-bold text-[#888] mt-1">
+            {movieDetails.Rated} - {movieDetails.Runtime} - {movieDetails.Genre}
+          </p>
+          <p className="text-md font-bold text-[#888]">
+            {movieDetails.Director} - {movieDetails.Writer} -{" "}
+            {movieDetails.Actors}
+          </p>
+          <p className="text-2xl font-bold text-[#ddd] my-12">
+            {movieDetails.Plot}
+          </p>
+          <p className="text-xl font-bold text-[#aaa]">Ratings</p>
+          <p className="text-md font-bold text-[#888]">
+            Imdb: {movieDetails.imdbRating} ({movieDetails.imdbVotes} votes)
+          </p>
+          <p className="text-md font-bold text-[#888]">
+            Metascore: {movieDetails.Metascore}
+          </p>
+        </div>
       </section>
     </main>
   );
